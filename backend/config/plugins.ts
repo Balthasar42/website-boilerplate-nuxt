33a622581145ba7aa198bbc0ef6ad10e572b9b02
Balthasar42 <zayn.wicke@gmail.com>
2023-11-02 10:51:53 +0100

    fix: removed graphql

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -2,16 +2,4 @@ export default {
   seo: {
     enabled: true,
   },
-  graphql: {
-    config: {
-      endpoint: "/graphql",
-      shadowCRUD: true,
-      playgroundAlways: false,
-      depthLimit: 7,
-      amountLimit: 100,
-      apolloServer: {
-        tracing: false,
-      },
-    },
-  },
 };
