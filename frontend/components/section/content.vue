377f33abb8774e251fc3dd971cb4ac4fca169e86
Balthasar42 <zayn.wicke@gmail.com>
2023-11-06 08:02:18 +0100

    feat: add markdown plugin

--- frontend/components/section/content.vue
+++ frontend/components/section/content.vue
@@ -1,3 +1,23 @@
 <template>
-  <div>Content</div>
+  <UContainer>
+    <h2>{{ section.title }}</h2>
+    <div class="prose" v-html="$markdown.render(section.content)" />
+  </UContainer>
 </template>
+
+<script setup lang="ts">
+interface ContentProps {
+  id: string;
+  title: string;
+  content: string;
+  // picture: Picture;
+  // buttons: Button[];
+}
+
+defineProps({
+  section: {
+    type: Object as PropType<ContentProps>,
+    default: () => null
+  }
+})
+</script>
