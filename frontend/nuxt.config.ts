2f529c284a3f90956fda5ac5848c3dc56b28fc23
Balthasar42 <zayn.wicke@gmail.com>
2023-11-07 11:42:23 +0100

    fix: eslint

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -6,29 +6,29 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   typescript: {
     strict: true,
-    typeCheck: true,
+    typeCheck: true
   },
   modules: [
     "@vueuse/nuxt",
     "@nuxt/ui",
     "@nuxtjs/strapi",
-    "@nuxtjs/google-fonts",
+    "@nuxtjs/google-fonts"
   ],
   strapi: {
-    url: process.env.STRAPI_URL || "http://127.0.0.1:1337",
+    url: process.env.STRAPI_URL || "http://127.0.0.1:1337"
   },
   ui: {
-    global: true,
+    global: true
   },
   tailwindcss: {
-    cssPath: resolve("assets/css/tailwind.css"),
+    cssPath: resolve("assets/css/tailwind.css")
   },
   googleFonts: {
     download: true,
     preload: true,
     families: {
       Outfit: true,
-      "Playfair+Display": true,
-    },
-  },
+      "Playfair+Display": true
+    }
+  }
 })
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -8,23 +8,23 @@ export default <Partial<Config>>{
         ...theme.fontFamily,
         serif: ["Playfair Display", ...theme.fontFamily.serif],
         sans: ["Outfit", ...theme.fontFamily.sans],
-        body: ["Outfit", ...theme.fontFamily.sans],
+        body: ["Outfit", ...theme.fontFamily.sans]
       },
       colors: {
         "waikawa-gray": {
-          "50": "#f2f7fb",
-          "100": "#e7f0f8",
-          "200": "#d3e2f2",
-          "300": "#b9cfe8",
-          "400": "#9cb6dd",
-          "500": "#839dd1",
-          "600": "#6a7fc1",
-          "700": "#6374ae",
-          "800": "#4a5989",
-          "900": "#414e6e",
-          "950": "#262c40",
-        },
-      },
-    },
-  },
+          50: "#f2f7fb",
+          100: "#e7f0f8",
+          200: "#d3e2f2",
+          300: "#b9cfe8",
+          400: "#9cb6dd",
+          500: "#839dd1",
+          600: "#6a7fc1",
+          700: "#6374ae",
+          800: "#4a5989",
+          900: "#414e6e",
+          950: "#262c40"
+        }
+      }
+    }
+  }
 }
